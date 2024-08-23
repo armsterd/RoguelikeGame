3e8483bbb8957a8a6f24ff5a43017efd647025ce
LEE, DONGEUN <43435779+armsterd@users.noreply.github.com>
2024-08-23 18:24:36 +0900

    Update game.js

--- game.js
+++ game.js
@@ -2,23 +2,66 @@ import chalk from 'chalk';
 import readlineSync from 'readline-sync';
 
 class Player {
-    constructor() {
-        this.hp = 100;
+    constructor(name, hp = 100) {
+        this.hp = hp;
+        this.name = name;
     }
 
-    attack() {
+    attack(target) {
         // 플레이어의 공격
+        console.log(this.name + '이 ' + target.name + ' 을 공격합니다.');
+        target.attacked(target);
+    }
+
+    attacked(damage) {
+        this.hp -= damage;
+        if (this.hp <= 0) {
+            console.log(this.name + '이' + '죽었습니다.');
+            continueBattle = false;
+        } else {
+            console.log(this.name + ' 님의 체력이 ' + this.hp + ' 남았습니다.');
+        }
+    }
+
+    run(percent) {
+        if (percent < 0.5) {
+            console.log(this.name + '은 도망치는데 실패했습니다.');
+        } else {
+            console.log(this.name + '은 도망쳤습니다.')
+        }
     }
 }
 
 class Monster {
-    constructor() {
-        this.hp = 100;
+    constructor(name, hp = 100) {
+        this.hp = hp;
+        this.name = name;
     }
 
-    attack() {
+    attack(target) {
         // 몬스터의 공격
+        console.log(this.name + '이 ' + target.name + ' 을 공격합니다.');
+        target.attacked(target);
     }
+
+    attacked(damage) {
+        this.hp -= damage;
+        if (this.hp <= 0) {
+            console.log(this.name + '이' + '죽었습니다.');
+            continueBattle = false;
+        } else {
+            console.log(this.name + ' 님의 체력이 ' + this.hp + ' 남았습니다.');
+        }
+    }
+
+    run(percent) {
+        if (percent < 0.5) {
+            console.log(this.name + '은 도망치는데 실패했습니다.');
+        } else {
+            console.log(this.name + '은 도망쳤습니다.')
+        }
+    }
+
 }
 
 function displayStatus(stage, player, monster) {
@@ -53,6 +96,14 @@ const battle = async (stage, player, monster) => {
 
         // 플레이어의 선택에 따라 다음 행동 처리
         logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+        switch (choice) {
+            case '1':
+                player.attack(monster);
+                break;
+            case '2':
+                player.run(Math.random());
+                break;
+        }
     }
 
 };
@@ -65,9 +116,13 @@ export async function startGame() {
     while (stage <= 10) {
         const monster = new Monster(stage);
         await battle(stage, player, monster);
-
+        const continueBattle = true;
         // 스테이지 클리어 및 게임 종료 조건
-
+        while (continueBattle) {
+            if (monster.hp > 0) {
+                monster.attack(player);
+            }
+        }
         stage++;
     }
 }
